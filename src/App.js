786c9966fd2e950c6a6d715ebea2afbf41e3fd5f
vidhusan12 <vidhusan709@gmail.com>
2025-07-17 18:37:16 +1200

    Added Styled Login and Signin components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Routes>
+          <Route path='/' element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
