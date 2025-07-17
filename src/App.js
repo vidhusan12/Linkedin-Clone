85b0e077fd41e5038ae01264ef9b8ecbb8ffe9fd
vidhusan12 <vidhusan709@gmail.com>
2025-07-17 19:23:48 +1200

    Implement responsive Login page with

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
+import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
@@ -8,6 +10,12 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<Login />} />
+          <Route path='/home' element={
+            <>
+              <Header />
+              <Home />
+            </>
+          } />
         </Routes>
       </Router>
     </div>
